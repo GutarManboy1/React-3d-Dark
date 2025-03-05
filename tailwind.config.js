24d547ebfeefa3c90c9a3f724a0be37c1dc5b644
Glenn Torrens <glenntorrens@gmail.com>
2025-03-05 23:25:09 +0900

    added hero components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx}"],
+    content: ["./src/**/*.{js,jsx}", "./index.html"],
     mode: "jit",
     theme: {
       extend: {
