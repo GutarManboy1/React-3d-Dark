fb3da005b6472b9f4f2b07b8441b0f6f24deae73
Glenn Torrens <glenntorrens@gmail.com>
2025-03-18 22:51:45 +0900

    removed react tilt dep, using spline for hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,41 @@
-import React from "react";
-import { motion } from "framer-motion";
+// import React from "react";
+// import { Spline } from "@splinetool/react-spline";
 
-import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+// import { motion } from "framer-motion";
 
-const Hero = () => {
+// import { styles } from "../styles";
+// import { ComputersCanvas } from "./canvas";
+
+// const Hero = () => {
+//   return (
+//     <section className="relative w-full h-screen mx-auto">
+//       <div
+//         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+//       >
+//         <div className="flex flex-col justify-center items-center mt-5">
+//           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+//           <div className="w-1 sm:h-80 h-40 violet-gradient" />
+//         </div>
+
+//         <Spline scene="https://prod.spline.design/zTjKXHxig970v6Uz/scene.splinecode" />
+//       </div>
+
+//       {/* <ComputersCanvas /> */}
+//     </section>
+//   );
+// };
+// export default Hero;
+import React from 'react';
+import Spline from '@splinetool/react-spline/next';
+
+
+const Hero = () =>{
   return (
-    <section className="relative w-full h-screen mx-auto">
-      <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
-      >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
-        </div>
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hey, I'm <span className="text-[#915eff]">Glenn</span>
-          </h1>
-        </div>
-        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I mostly break stuff, <br className="sm:block hidden" />
-          freak out and eventually make them work. Let's make something
-          together!
-        </p>
-      </div>
-
-      <ComputersCanvas />
+    <section style={{ width: '100%', height: '100vh' }}>
+      <Spline
+        scene="https://prod.spline.design/zTjKXHxig970v6Uz/scene.splinecode" 
+      />
     </section>
   );
-};
-
-export default Hero;
+}
+export default Hero 
