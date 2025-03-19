6648214084fafbf3e8a3737ce737213f3ee09708
Glenn Torrens <glenntorrens@gmail.com>
2025-03-19 18:57:23 +0900

    spline works, updated everything and the returned deps to priviouss state

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,41 +1,41 @@
-// import React from "react";
-// import { Spline } from "@splinetool/react-spline";
+import React from "react";
+import Spline from "@splinetool/react-spline";
+import { motion } from "framer-motion";
 
-// import { motion } from "framer-motion";
+import { styles } from "../styles";
+import SplineScene from "./SplineScene";
 
-// import { styles } from "../styles";
-// import { ComputersCanvas } from "./canvas";
-
-// const Hero = () => {
-//   return (
-//     <section className="relative w-full h-screen mx-auto">
-//       <div
-//         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
-//       >
-//         <div className="flex flex-col justify-center items-center mt-5">
-//           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-//           <div className="w-1 sm:h-80 h-40 violet-gradient" />
-//         </div>
-
-//         <Spline scene="https://prod.spline.design/zTjKXHxig970v6Uz/scene.splinecode" />
-//       </div>
-
-//       {/* <ComputersCanvas /> */}
-//     </section>
-//   );
-// };
-// export default Hero;
-import React from 'react';
-import Spline from '@splinetool/react-spline/next';
-
-
-const Hero = () =>{
+const Hero = () => {
   return (
-    <section style={{ width: '100%', height: '100vh' }}>
-      <Spline
-        scene="https://prod.spline.design/zTjKXHxig970v6Uz/scene.splinecode" 
-      />
+    <section className="relative w-full h-screen mx-auto">
+      <div className="absolute inset-0 top-[120px] max-w-7xl mx-auto">
+        <h1 className={`${styles.heroHeadText} mt-5`}>
+          Hey, I'm <span className="text-[#915EFF]">Glenn</span>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a Software Engineer, <br className="sm:block hidden" />
+            that makes and breaks tons of stuff!
+          </p>
+        </h1>
+      </div>
+      <SplineScene />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center ">
+        <a href="#about ">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
-}
-export default Hero 
+};
+export default Hero;
